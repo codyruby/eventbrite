c5b5aa697d278be91d3784053c29838fd47093c7
Yousri <laifayousri@gmail.com>
2019-05-10 13:55:40 +0200

    Modification du model Attendance

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,4 +1,11 @@
 class Attendance < ApplicationRecord
     belongs_to :user
     belongs_to :event
+
+    after_create_commit :welcome_event_send
+
+    def welcome_event_send
+      UserMailer.welcome_event_email(self.event, self.user).deliver_now
+    end  
+
 end
