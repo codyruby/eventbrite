eeb989110168219e80c105d32655411743a8baaf
Yousri <laifayousri@gmail.com>
2019-05-14 15:28:35 +0200

    Modification du controller events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,25 @@
 class EventsController < ApplicationController
   def index
+    @events = Event.all
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
   end
 end
