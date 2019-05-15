f92ce420a6929f4edf8d30fe4cb55260e915d9c0
Yousri <laifayousri@gmail.com>
2019-05-15 18:26:58 +0200

    Modification du controller events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,6 @@
 class EventsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+  
   def index
     @events = Event.all
   end
@@ -11,7 +13,15 @@ class EventsController < ApplicationController
     @event = Event.new
   end
 
+  # Méthode qui permet de créer un nouvel évènement avec les params récupéré dans le formulaire de la vue new de events
   def create
+    @event = Event.new(event_params.merge(user_id: current_user.id))
+    if @event.save
+    
+    redirect_to event_path(id: @event.id)
+    else
+    render 'new'
+    end
   end
 
   def edit
@@ -22,4 +32,11 @@ class EventsController < ApplicationController
 
   def destroy
   end
+
+  private
+
+  def event_params
+    p params.require(:event).permit(:start_date, :duration, :description, :title, :price, :location)
+  end
+
 end
