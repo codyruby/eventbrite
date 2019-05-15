1b941625fcd479e6da75a2416be868142cef626b
Yousri <laifayousri@gmail.com>
2019-05-15 15:00:07 +0200

    Modification du controller users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,18 +7,4 @@ class UsersController < ApplicationController
     @events = Event.where(user_id: @user.id)
   end
 
-  def edit
-    @user = current_user
-  end
-
-  def update
-    @user = current_user
-     if @user.update(user_params) 
-
-      redirect_to profil_path
-    else
-      render :edit
-    end
-  end
-
 end
