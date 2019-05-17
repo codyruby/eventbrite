08b6d718fa11980961d6a531fafd495f30761151
Yousri <laifayousri@gmail.com>
2019-05-17 13:05:32 +0200

    Mofication des routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,7 @@ Rails.application.routes.draw do
   devise_for :users 
   
   resources :events
- 
-
+  
   root 'events#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
