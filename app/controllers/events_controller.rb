b5f1f6fb8805e36ebcb9a7622d1f900ba1b01bc2
Yousri <laifayousri@gmail.com>
2019-05-17 17:13:52 +0200

    Ajout de la méthode subscribe

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -23,6 +23,34 @@ class EventsController < ApplicationController
     render 'new'
     end
   end
+ 
+  def subscribe 
+    @event = Event.find(params[:id])
+
+    @amount = @event.price * 100
+
+    customer = Stripe::Customer.create({
+      email: params[:stripeEmail],
+      source: params[:stripeToken],
+    })
+  
+    charge = Stripe::Charge.create({
+      customer: customer.id,
+      amount: @amount,
+      description: 'Rails Stripe customer',
+      currency: 'EUR',
+    })
+
+    # Création d'une participation d'un user à l'évènement
+    @attendance = Attendance.create(stripe_customer_id: customer.id, user: current_user, event: @event)
+    
+    redirect_to event_path(@event.id)
+  
+  rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to @event
+
+  end
 
   def edit
   end
