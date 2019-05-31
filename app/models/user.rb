258e508c9106313a4c315443328333c5eb682295
Yousri <laifayousri@gmail.com>
2019-05-31 17:55:50 +0200

    Ajout avatar user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,13 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+    devise :database_authenticatable, :registerable,
+           :recoverable, :rememberable, :validatable
     has_many :events     
     has_many :attendances
     has_many :events, through: :attendances
     after_create :welcome_send
+    has_one_attached :avatar
 
     def welcome_send
       UserMailer.welcome_email(self).deliver_now
