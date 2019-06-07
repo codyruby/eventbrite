860572dcc7ebc03413f267ae6dffc783c6b12cd5
Yousri <laifayousri@gmail.com>
2019-06-07 20:20:47 +0200

    Ajout d'une méthode check_if_admin dans le controller admin

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,4 +1,39 @@
 class Admin::AdminController < ApplicationController
+  before_action :check_if_admin
+
     def index
+      @users = User.all
     end
-  end 
+
+    def show
+      @user = User.find(params[:id])
+    end
+
+    def new
+      @user = User.new
+    end
+
+    def create 
+      
+    end
+
+    def edit 
+
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    private
+
+    def check_if_admin
+      unless current_user.admin == true
+        flash[:error] = "Vous n'êtes pas le créateur de l'évènement => Impossilble de le modifier."
+       redirect_to root_path
+      end
+    end
+
+end 
