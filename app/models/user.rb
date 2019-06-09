03ccfc93994f41a9c5f98851491aea8870022517
Yousri <laifayousri@gmail.com>
2019-06-09 19:14:15 +0200

    Modification du model User

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
     devise :database_authenticatable, :registerable,
            :recoverable, :rememberable, :validatable
-    has_many :events     
+    has_many :events, dependent: :destroy    
     has_many :attendances
-    has_many :events, through: :attendances
+    has_many :events, through: :attendances, dependent: :destroy
     after_create :welcome_send
     has_one_attached :avatar
 
