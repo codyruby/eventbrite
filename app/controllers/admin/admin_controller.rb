ec53827794e9524348cb89152858bcd1ad4cc85b
Yousri <laifayousri@gmail.com>
2019-06-09 19:12:52 +0200

    Modification du controller admin

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -14,17 +14,35 @@ class Admin::AdminController < ApplicationController
     end
 
     def create 
+      @user = User.new(user_params)
+ 
+      if @user.save
+        redirect_to admin_root
+      else
+        render 'new'
+      end
       
     end
 
     def edit 
-
+      @user = User.find(params[:id])
     end
 
     def update
+      @user = User.find(params[:id])
+ 
+      if @user.update(user_params)
+        redirect_to admin_root
+      else
+        render 'edit'
+      end
     end
 
     def destroy
+      @user = User.find(params[:id])
+      @user.destroy
+    
+      redirect_to admin_path
     end
 
     private
@@ -34,6 +52,10 @@ class Admin::AdminController < ApplicationController
         flash[:error] = "Vous n'êtes pas le créateur de l'évènement => Impossilble de le modifier."
        redirect_to root_path
       end
+    end
+
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :description)
     end
 
 end 
